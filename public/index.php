71b01da8e5de7f23c0906fea7a148adf08eea809
LeandroBaroni <leandrobaroni2103@hotmail.com>
2021-03-05 03:41:04 -0300

    Primeiro Commit

---
+++ public/index.php
@@ -0,0 +1,7 @@
+<?php
+require_once(dirname(__FILE__, 2) . '/src/config/config.php');
+require_once(dirname(__FILE__, 2) . '/src/models/User.php');
+
+$user = new User(['name' => 'Lucas', 'email' => 'lucas@gmail.com']);
+print_r($user);
+echo 'FIM';
