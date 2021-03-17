038e27745a15c3d53e513cf270bd2db7b62625f5
Leandro Baroni <leandrobaroni2103@hotmail.com>
2021-03-17 07:05:28 -0300

    Tela de Relatorio Mensal

--- src/controllers/monthly_report.php
+++ src/controllers/monthly_report.php
@@ -37,6 +37,6 @@ $sign = ($sumOfWorkedTime >= $expectedTime) ? '+' : '-';
 
 loadTemplateView('monthly_report', [
     'report' => $report,
-    'sumOfWorkedTime' => $sumOfWorkedTime,
+    'sumOfWorkedTime' => getTimeStringFromSeconds($sumOfWorkedTime),
     'balance' => "{$sign}{$balance}"
 ]);
--- src/views/monthly_report.php
+++ src/views/monthly_report.php
@@ -1,12 +1,40 @@
 <main class="content">
-<?php
-print_r($report);
-echo "<hr>";
-echo $sumOfWorkedTime;
-echo "<hr>";
-echo $balance;
-// foreach($registries as $values){
-//     echo "$values->work_date, $values->time1, $values->time2, $values->time3, $values->time4, $values->worked_time<br>";
-// }
-?>
+    <?php
+        renderTitle(
+            'Relatorio Mensal',
+            'Acompanhe seu saldo de horas',
+            'icofont-ui-calendar'
+        );
+    ?>
+
+    <div>
+        <table class="table table-bordered table-striped table-hover">
+            <thead class="cabecalho">
+                <th>Dia</th>
+                <th>Entrada 1</th>
+                <th>Saída 1</th>
+                <th>Entrada 2</th>
+                <th>Saída 2</th>
+                <th>Saldo</th>
+            </thead>
+            <tbody class="corpo">
+                <?php foreach ($report as $registry): ?>
+                <tr>
+                    <td><?= formatDateWithLocale($registry->work_date, '%A, %d de %B de %Y') ?></td>
+                    <td><?= $registry->time1 ?></td>
+                    <td><?= $registry->time2 ?></td>
+                    <td><?= $registry->time3 ?></td>
+                    <td><?= $registry->time4 ?></td>
+                    <td><?= $registry->getBalance() ?></td>
+                </tr>
+                <?php endforeach?>
+                <tr class="bg-primary text-white">
+                    <td>Horas Trabalhadas</td>
+                    <td colspan="3"><?= $sumOfWorkedTime ?></td>
+                    <td>Saldo Mensal</td>
+                    <td><?= $balance ?></td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
 </main>
